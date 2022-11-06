1095c964e87b91f6615661d9c072407dd8b3a4ad
dlord-20 <dthomaslord@gmail.com>
2022-11-06 10:00:09 +0300

    Completed basic backend but can add more (delete, etc.)

--- files/server.js
+++ files/server.js
@@ -5,8 +5,42 @@ const app = express();
 const { quotes } = require('./data');
 const { getRandomElement } = require('./utils');
 
-const PORT = process.env.PORT || 4001;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static('public'));
 
-//I'm going back so I better understand express + this is what i'm talking about -- One day life will get back to normal - back to coding -- postman! -- postman on my desktop! -- MolEvo Interface right now
+app.get('/api/quotes/random', (req, res) => {
+  res.send({
+    quote: getRandomElement(quotes)
+  });
+});
+
+app.get('/api/quotes', (req, res) => {
+  if (req.query.person !== undefined) {
+    const quotesByPerson = quotes.filter(quote => quote.person === req.query.person);
+    res.send({
+      quotes: quotesByPerson
+    });
+  } else {
+    res.send({
+      quotes: quotes
+    });
+  }
+});
+
+app.post('/api/quotes', (req, res) => {
+  const newQuote = {
+    quote: req.query.quote,
+    person: req.query.person
+  };
+  if (newQuote.quote && newQuote.person) {
+    quotes.push(newQuote);
+    res.send({ quote: newQuote });
+  } else {
+    res.status(400).send();
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}.`);
+});
